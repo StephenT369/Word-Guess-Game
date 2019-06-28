e135272c0616b0dd5c0a312f18227b2194709f15
Stephen Thompson <smt3@hotmail.com>
2019-06-28 00:38:49 -0500

    added blanks array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 //Stephen Thompson \^/
 
 var guessGame = {
-    
     currWord:
         ['sun', 'mercury', 'venus', 'earth', 'moon', 'mars', 'jupiter', 'saturn', 'uranus']
         [Math.floor(Math.random()*
@@ -9,11 +8,26 @@ var guessGame = {
         .length)],
     numGuess: 0,
     numGuessRm: 15,
+    currWordSpt: function(){
+        document.write(this.currWord);
+    },
+    currWordBlanks: function(){
+        var ansKey = [];
+        for (var a = 0; a < this.currWord.length; a++) {
+            ansKey[a] = " _ ";
+        };
+        document.write(ansKey.join('&nbsp'));
+        console.log(ansKey.join(' '))
+    }
+
+
 
 };
 
    
     console.log(guessGame.currWord);
+    guessGame.currWordSpt();
+    guessGame.currWordBlanks();
 
 
 
